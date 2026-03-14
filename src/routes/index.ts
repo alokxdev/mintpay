cacd2481fa8c59b6348c23be2a5158953267705c
Alok <alokforreal@gmail.com>
2026-03-14 14:55:59 +0530

    register group routes in routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import authRoutes from "../modules/auth/auth.routes.js";
 import userRoutes from "../modules/user/user.routes.js";
+import groupRoutes from "../modules/group/group.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/groups", groupRoutes);
 
 export default router;
