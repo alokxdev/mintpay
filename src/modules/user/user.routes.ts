0226080aa621255c8af394e5304ea63dfca33bb7
Alok <alokforreal@gmail.com>
2026-03-23 17:53:57 +0530

    refactor(settlement): remove settlement.routes.ts
    - move GET /:groupId/balances into group.routes.ts
    - move GET /me/balances into user.routes.ts

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "../../middleware/auth.middleware.js";
 import { updateUserSchema, deleteUserSchema } from "./user.schema.js";
 
 import { getMe, updateMe, deleteMe } from "./user.controller.js";
+import { getUserBalancesController } from "../settlement/settlement.controller.js";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use(authMiddleware);
 router.get("/me", getMe);
 router.patch("/me", validate({ body: updateUserSchema }), updateMe);
 router.delete("/me", validate({ body: deleteUserSchema }), deleteMe);
+router.get("/me/balance", getUserBalancesController);
 
 export default router;
